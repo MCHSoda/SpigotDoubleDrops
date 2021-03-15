a8e852192d100f047480c451b2818c9cba80a5fb
MCHSoda <MCHGECKO@GMAIL.COM>
2021-03-15 10:26:03 -0400

    added main class

---
+++ src/com/ffc/spigotplugins/mchsoda/doubledrops/Main.java
@@ -0,0 +1,17 @@
+package com.ffc.spigotplugins.mchsoda.doubledrops;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
